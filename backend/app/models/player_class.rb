f13b8906818277f42e2726d514fadb7f199c368c
Egregious <dvdscheibel@yahoo.com>
2021-07-06 10:45:28 -0400

    models update

--- backend/app/models/player_class.rb
+++ backend/app/models/player_class.rb
@@ -1,4 +1,5 @@
 class PlayerClass < ApplicationRecord
+    belongs_to :builds
     has_many :specs
     has_many :talents, through: :specs
     has_many :ranks, through: :talents
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_many :builds
+    has_many :player_classes, through: :builds
+    has_many :specs, through: :player_classes
+    has_many :talents, through: :specs 
+    has_many :ranks, through: :talents
 end
